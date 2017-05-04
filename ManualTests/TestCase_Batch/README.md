e8193ed8587afc76586a885ee16e142984b7a224
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2017-05-04 12:32:33 +0200

    Add batch regression test

---
+++ ManualTests/TestCase_Batch/README.md
@@ -0,0 +1,6 @@
+Windows
+=======
+
+To run the manual test, execute `runtests.bat <resinsight.exe> [<compare path>]`, then open `Report.html` to verify output.
+
+To generate diffs between the images, the `<compare path>` argument must be given. This should be the folder where `compare` is.
---
+++ ManualTests/TestCase_Batch/runtests.bat
@@ -0,0 +1,42 @@
+@echo off
+rem Absolute path to ResInsight.exe (first parameter)
+set resinsight=%~dpnx1
+rem Absolute path to project directory
+set regtestdir=%~dp0
+rem Move to project directory to execute
+pushd %regtestdir%
+
+@echo Running multiCaseSnapshots
+%resinsight% --project %regtestdir%\RegressionTest.rsp --multiCaseSnapshots %regtestdir%\Caselist.txt --size 1024 800
+@echo Running replaceCase
+%resinsight% --project %regtestdir%\RegressionTest.rsp --replaceCase "../../ModelData/Case_with_10_timesteps/Real0/BRUGGE_0000.EGRID" --savesnapshots --size 1024 800
+@echo Running replaceSourceCases
+%resinsight% --project %regtestdir%\MultiCaseRegressionTest.rsp --replaceSourceCases %regtestdir%\MultiCaseList.txt --savesnapshots --size 1024 800
+
+if [%2]==[] goto end
+
+if exist %2 (
+	@echo Running diffing
+	if not exist "RegTestDiffImages" mkdir "RegTestDiffImages"
+	if not exist "RegTestDiffImages/snapshots" mkdir "RegTestDiffImages/snapshots"
+	if not exist "RegTestDiffImages/multiCaseSnapshots" mkdir "RegTestDiffImages/multiCaseSnapshots"
+	pushd %2
+	for %%i in (
+		snapshots/Real0--BRUGGE_0000_EGRID-View_1.png
+		snapshots/Statistics_1-View_1.png
+		multiCaseSnapshots/Real0--BRUGGE_0000_EGRID-View_1.png
+		multiCaseSnapshots/Real10--BRUGGE_0010_EGRID-View_1.png
+		multiCaseSnapshots/Real30--BRUGGE_0030_EGRID-View_1.png
+		multiCaseSnapshots/Real40--BRUGGE_0040_EGRID-View_1.png
+	) do (
+		compare -fuzz 0.4%% -lowlight-color white -metric ae "%regtestdir%BaseImages/%%i" "%regtestdir%%%i" "%regtestdir%RegTestDiffImages/%%i"
+	)
+	popd
+) else (
+	@echo Could not find compare at %2
+)
+
+:end
+
+rem Return to original executing directory
+popd
