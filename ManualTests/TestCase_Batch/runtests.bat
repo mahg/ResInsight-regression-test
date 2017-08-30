cb85283746f401e260f8579f06742f7f7132789e
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2017-08-30 14:21:24 +0200

    Fixed manual batch test case after snapshot filename renaming.

--- ManualTests/TestCase_Batch/runtests.bat
+++ ManualTests/TestCase_Batch/runtests.bat
@@ -22,12 +22,12 @@ if exist %2 (
 	if not exist "RegTestDiffImages/multiCaseSnapshots" mkdir "RegTestDiffImages/multiCaseSnapshots"
 	pushd %2
 	for %%i in (
-		snapshots/Real0--BRUGGE_0000_EGRID-View_1.png
-		snapshots/Statistics_1-View_1.png
-		multiCaseSnapshots/Real0--BRUGGE_0000_EGRID-View_1.png
-		multiCaseSnapshots/Real10--BRUGGE_0010_EGRID-View_1.png
-		multiCaseSnapshots/Real30--BRUGGE_0030_EGRID-View_1.png
-		multiCaseSnapshots/Real40--BRUGGE_0040_EGRID-View_1.png
+		snapshots/Real0--BRUGGE_0000_EGRID_View_1_PERMZ_00_01-jan_1997.png
+		snapshots/Statistics_1_View_1_PORO_RANGE_00_01-jan_1997.png
+		multiCaseSnapshots/Real0--BRUGGE_0000_EGRID_View_1_PERMZ_00_01-jan_1997.png
+		multiCaseSnapshots/Real10--BRUGGE_0010_EGRID_View_1_PERMZ_00_01-jan_1997.png
+		multiCaseSnapshots/Real30--BRUGGE_0030_EGRID_View_1_PERMZ_00_01-jan_1997.png
+		multiCaseSnapshots/Real40--BRUGGE_0040_EGRID_View_1_PERMZ_00_01-jan_1997.png
 	) do (
 		compare -fuzz 0.4%% -lowlight-color white -metric ae "%regtestdir%BaseImages/%%i" "%regtestdir%%%i" "%regtestdir%RegTestDiffImages/%%i"
 	)
